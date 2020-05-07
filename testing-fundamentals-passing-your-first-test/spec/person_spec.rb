6b33159d72ecccc421a4a2b3a696df39981932dd
Mike Warren <mikeywarren89@gmail.com>
2020-05-07 18:09:39 +0900

    got test working

--- testing-fundamentals-passing-your-first-test/spec/person_spec.rb
+++ testing-fundamentals-passing-your-first-test/spec/person_spec.rb
@@ -3,9 +3,8 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      person = Person.new("John", "Smith")
+      expect(person.full_name).to eq "John Smith"
     end
   end
 end
